173b8ba9fb52c364022ecd0371291b43e4fc8d81
ManuelCortez <mariomanuelcortezgrano@gmail.com>
2023-01-16 17:00:30 -0600

    16/01/2023 - CiudadServiceImpl.java - se actualizo en DTO al uso de mapper

--- AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/service/impl/CiudadServiceImpl.java
+++ AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/service/impl/CiudadServiceImpl.java
@@ -5,43 +5,47 @@ import mx.com.basantader.AgenciaViajeTD.exceptions.BusinessException;
 import mx.com.basantader.AgenciaViajeTD.model.CiudadEntity;
 import mx.com.basantader.AgenciaViajeTD.repository.CiudadRepository;
 import mx.com.basantader.AgenciaViajeTD.service.CiudadService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+/**
+ * @author MarioManuelCorezGrano
+ */
 @Service
 public class CiudadServiceImpl implements CiudadService {
 
     @Autowired
     private CiudadRepository ciudadRepository;
+    private ModelMapper mapper;
 
     @Override
     public List<CiudadDto> getAllCiudades() {
-        List<CiudadEntity> listaCiudadesEntity = ciudadRepository.findAll();
-        List<CiudadDto> listaCiudadesDto = new ArrayList<CiudadDto>();
-        CiudadDto ciudadDto;
-        for (CiudadEntity ciudadEntity: listaCiudadesEntity){
-            ciudadDto = new CiudadDto();
-            ciudadDto.setIdCiudad(ciudadEntity.getIdCiudad());
-            ciudadDto.setNombreCiudad(ciudadEntity.getNombreCiudad());
-            listaCiudadesDto.add(ciudadDto);
-        }
+
+        mapper = new ModelMapper();
+
+        List<CiudadDto> listaCiudadesDto = ciudadRepository.findAll().stream()
+                .map(ciudadEntity -> mapper.map(ciudadEntity, CiudadDto.class))
+                .collect(Collectors.toList());
+
         return listaCiudadesDto;
     }
 
     @Override
     public CiudadDto getCiudadbyName(String nombreCiudad) {
+
+        mapper = new ModelMapper();
+
         Optional<CiudadEntity> ciudadEntity = Optional.ofNullable(ciudadRepository.findByNombreCiudad(nombreCiudad));
         if (!ciudadEntity.isPresent()){
             throw new BusinessException(5);
         }
-        CiudadDto ciudadDto = new CiudadDto();
-
-        ciudadDto.setIdCiudad(ciudadEntity.get().getIdCiudad());
-        ciudadDto.setNombreCiudad(ciudadEntity.get().getNombreCiudad());
+        CiudadDto ciudadDto = this.mapper.map(ciudadEntity, CiudadDto.class);
 
         return ciudadDto;
     }
