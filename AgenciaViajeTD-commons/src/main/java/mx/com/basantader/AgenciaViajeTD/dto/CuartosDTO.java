3581b9f464f81f3f0e5b094927f1267675fa78b9
FLORES ALLENDE <carlos.flores@capgemini.com>
2023-01-17 15:56:32 -0600

    Creacion de modelo Cuartos

---
+++ AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/CuartosDTO.java
@@ -0,0 +1,26 @@
+package mx.com.basantader.AgenciaViajeTD.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CuartosDTO {
+
+  private Long idcuarto;
+  private String nombreCuarto;
+  private String Descripcion;
+  private Long numeroPersonas;
+  private String codigoCuartos;
+  private Float costoNoche;
+  private String tipoCuarto;
+
+  //Relacion con tabla Hotel
+  private Long idHotel;
+
+}
---
+++ AgenciaViajeTD-persistence/src/main/java/mx/com/basantader/AgenciaViajeTD/model/CuartosEntity.java
@@ -0,0 +1,45 @@
+package mx.com.basantader.AgenciaViajeTD.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CUARTOS")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CuartosEntity {
+
+    @Id
+    @SequenceGenerator(name = "sequ", sequenceName = "application_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(generator = "sequ")
+    private Long idcuarto;
+
+    @Column(name = "NOMBRE_CUARTO", nullable = false)
+    private String nombreCuarto;
+
+    @Column(name = "DESCRIPCION", nullable = false)
+    private String Descripcion;
+
+    @Column(name = "NUMERO_PERSONAS", nullable = false)
+    private Long numeroPersonas;
+
+    @Column(name = "CODIGO_CUARTO", nullable = false)
+    private String codigoCuartos;
+
+    @Column(name = "COSTO_NOCHE", nullable = false)
+    private Float costoNoche;
+
+    @Column(name = "TIPO_CUARTO", nullable = false)
+    private String tipoCuarto;
+
+    //@ManyToOne
+    //@JoinColumn(name = "ID_HOTEL")
+    //private HotelEntity hotel;
+
+}
