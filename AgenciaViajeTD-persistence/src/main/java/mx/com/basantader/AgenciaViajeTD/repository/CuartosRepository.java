9325772d67628e8e7c4dc8e83d5bf74e4afb6a67
FLORES ALLENDE <carlos.flores@capgemini.com>
2023-01-18 18:46:35 -0600

    Creacion de Crear Cuartos

--- AgenciaViajeTD-persistence/src/main/java/mx/com/basantader/AgenciaViajeTD/repository/CuartosRepository.java
+++ AgenciaViajeTD-persistence/src/main/java/mx/com/basantader/AgenciaViajeTD/repository/CuartosRepository.java
@@ -2,6 +2,12 @@ package mx.com.basantader.AgenciaViajeTD.repository;
 
 import mx.com.basantader.AgenciaViajeTD.model.CuartosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface CuartosRepository extends JpaRepository<CuartosEntity, Long> {
+   Optional<CuartosEntity> finByNombreCuarto(String validarNC);
+  // Optional<CuartosEntity> finByCodigoCuarto(String validarCC);
 }
