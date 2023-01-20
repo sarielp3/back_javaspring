bb25182fb039a1f3d0f4d5960fe9eb8e96adaf8b
ManuelCortez <mariomanuelcortezgrano@gmail.com>
2023-01-20 16:12:16 -0600

    Servicio POST de vuelos creado, se agregaron logs y anotaciones en los controlles

---
+++ AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/AltaVueloDto.java
@@ -0,0 +1,32 @@
+package mx.com.basantader.AgenciaViajeTD.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Time;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AltaVueloDto {
+    private Long idVuelo;
+
+    private Long origen;
+
+    private Long destino;
+
+    private Long aerolinea;
+
+    private String estatus;
+
+    private Time horaSalida;
+
+    private Time horaLlegada;
+
+    private String codigoVuelo;
+
+    private Float costo;
+}
