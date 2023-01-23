38f7734a15185240332d003b0f060030f308dab0
ManuelCortez <mariomanuelcortezgrano@gmail.com>
2023-01-23 16:40:54 -0600

    Se agrego la anotacion @CrossOrigin a los controllers de ciudades, vuelos y aerolineas

--- AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/controller/CiudadController.java
+++ AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/controller/CiudadController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/ciudades")
 @Api(value = "Endpoints para obtener listas ciudades")
+@CrossOrigin(origins = "http://localhost:4200" )
 public class CiudadController {
 
     @Autowired
--- AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/controller/VueloController.java
+++ AgenciaViajeTD-services/src/main/java/mx/com/basantader/AgenciaViajeTD/controller/VueloController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/vuelos")
 @Api(value = "Endpoins para administracion de vuelos")
+@CrossOrigin(origins = "http://localhost:4200" )
 public class VueloController {
 
     @Autowired
