ff5b1923e9446a450ea8098b6a2a3bf13f25f2f2
LECHUGA <felipe.lechuga@capgemini.com>
2023-01-30 10:56:05 -0600

    agregar altaDto y modificacion de consulta GET

--- AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/ReservaDto.java
+++ AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/ReservaDto.java
@@ -1,36 +1,19 @@
 package mx.com.basantader.AgenciaViajeTD.dto;
 
 import java.util.Date;
-
 import lombok.*;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class ReservaDto {
 	
-	private Date fechaInicio;
-	private Date fechaFin;
-	private Long idReserva;
+	private VueloDto vuelo;
 	private String nombreCliente;
 	private String apellidoPaternoCliente;
 	private String apellidoMaternoCliente;
 	private String descripcion;
-	private HotelDto hotel;
-	private CuartoDto cuarto;
-	private VueloDto vuelo;
 	private Date fechaCreacion;
-	
-	
-	private Long idHotel;
-	
-	
-	private Long idCuarto;
-	
-	
-	private Long idVuelo;
-	
 
 }
