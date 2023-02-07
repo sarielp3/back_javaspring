7aee9bc64ea245ac2caff8383b97df1884740290
ManuelCortez <mariomanuelcortezgrano@gmail.com>
2023-02-07 15:59:58 -0600

    Validacion en longitaud de atributos descripcion, apellido materno, apellido paterno y nombre de AltaReservaDto.

--- AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/AltaReservaDto.java
+++ AgenciaViajeTD-commons/src/main/java/mx/com/basantader/AgenciaViajeTD/dto/AltaReservaDto.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.*;
 
@@ -27,15 +28,19 @@ public class AltaReservaDto implements Serializable{
 	private Date fechaFin;
 	
 	@NotNull(message = "El nombre del cliente no puede ser nulo")
+	@Size(min = 1, max = 30, message = "El nombre solo puede contener entre 1 y 30 caracteres")
 	private String nombreCliente;
 	
 	@NotNull(message = "El apellido paterno del cliente no puede ser nulo")
+	@Size(min = 1, max = 30, message = "El apellido materno solo puede contener entre 1 y 30 caracteres")
 	private String apellidoPaternoCliente;
 	
 	@NotNull(message = "El apellido materno del cliente no puede ser nulo")
+	@Size(min = 1, max = 30, message = "El apellido materno solo puede contener entre 1 y 30 caracteres")
 	private String apellidoMaternoCliente;
 	
 	@NotNull(message = "La descripcion no puede ser nula")
+	@Size(min = 1, max = 300, message = "La descripcion solo puede contener entre 1 y 300 caracteres")
 	private String descripcion;
 
 	private Date fechaCreacion;
